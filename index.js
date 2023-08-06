65f303a32d0ae8b326a24715b2e4c5b97aba2071
nitesh <niteshkymar1@gmail.com>
2023-08-06 22:25:13 +0530

    after connecting database

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3200
+const mongoose = require('mongoose')
 
+mongoose.connect("mongodb+srv://97nitesh85:85nitesh97collectdata@cluster0.oegqizs.mongodb.net/collectdata",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{console.log("mongodb connect")}).catch(()=>{console.log("not connect mongodb")})
+
+const userschema = new mongoose.Schema({
+    name:String,
+    city:String
+})
+
+const usermodel = new mongoose.model("User",userschema)
 app.get("/",(req,res)=>{
     res.status(201).send("this is home page")
 })
 
 app.get("/about",(req,res)=>{
     res.status(201).send("this is about page")
+})
+
+app.get("/fetch",async (req,res)=>{
+    const data = await usermodel.find()
+    res.status(201).send(data)
 })
 
 app.listen(PORT,()=>{
