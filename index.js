ea3d1e7c13f944b0dbc39052cb53b5f81516b7f4
nitesh <niteshkymar1@gmail.com>
2023-08-06 21:58:18 +0530

    djfsjdlf

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3200
 
 app.get("/",(req,res)=>{
     res.status(201).send("this is home page")
